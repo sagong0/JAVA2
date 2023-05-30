bfa56f889e9dc1f94260ae30f37149467d3aa224
sagong0 <1w2q3e@naver.com>
2023-05-30 21:18:31 +0900

    singleton

---
+++ chap07/src/sec01/exam01/CellPhone.java
@@ -0,0 +1,21 @@
+package sec01.exam01;
+
+public class CellPhone {
+	// 필드
+	String model;
+	String color;
+	
+	//생성자
+//	CellPhone(){}
+	public CellPhone(String model, String color){
+		this.model = model;
+		this.color = color;
+	}
+	//method
+	void powerOn() {System.out.println("전원을 켭니다");}
+	void powerOff() {System.out.println("전원을 끕니다");}
+	void bell() {System.out.println("벨이 울립니다.");}
+	void sendVoicd(String message) {System.out.println("자기: " + message);}
+	void receivveVoice(String message) {System.out.println("상대방: " + message);}
+	void hangUp() {System.out.println("전화를 끊습니다.");}
+}
---
+++ chap07/src/sec01/exam01/DmbPhone.java
@@ -0,0 +1,13 @@
+package sec01.exam01;
+
+public class DmbPhone extends CellPhone{
+	
+	int channel;
+
+	public DmbPhone(String model, String color, int channel) {
+		super(model, color);
+		this.channel = channel;
+
+	}
+
+}
